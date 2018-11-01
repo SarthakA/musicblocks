7370f872dfc1902a93da834ddaef438808e55173
SarthakA <trapti240@gmail.com>
2018-11-01 17:54:30 +0530

    harp.js

---
+++ sounds/samples/harp.js
@@ -0,0 +1,5 @@
+//source:https://freesound.org/people/Meral/sounds/104588/ and licence : Creative Commons 0 License
+
+HARP_SAMPLE = function () {
+ return "data:audio/wav;base64,harp.b64encoded";
+};
