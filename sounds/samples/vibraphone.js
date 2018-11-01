5cc3e8b02b38269fa34d1ca666414434edc67e06
SarthakA <trapti240@gmail.com>
2018-11-01 17:53:52 +0530

    vibraphone.js

---
+++ sounds/samples/vibraphone.js
@@ -0,0 +1,5 @@
+//source : https://freesound.org/people/arseniiv/sounds/238745/ and licence : Creative Commons 0 License
+
+VIBRAPHONE_SAMPLE = function () {
+ return "data:audio/wav;base64,vibraphone.b64encoded";
+};
