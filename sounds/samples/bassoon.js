ad720b539b515560b9919faed6953f026e85a123
SarthakA <trapti240@gmail.com>
2018-11-01 17:55:49 +0530

    Create bassoon.js

---
+++ sounds/samples/bassoon.js
@@ -0,0 +1,5 @@
+//source : https://freesound.org/people/Carlos_Vaquero/sounds/154329/ and license : Attribution Noncommercial License
+
+BASSOON_SAMPLE = function () {
+ return "data:audio/wav;base64,Bassoon.b64encoded";
+};
