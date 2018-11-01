00daf959aaf8b37975f448920170fd89c736173c
SarthakA <trapti240@gmail.com>
2018-11-01 17:52:59 +0530

    xylophone.js

---
+++ sounds/samples/xylophone.js
@@ -0,0 +1,5 @@
+//source:https://freesound.org/people/Tristan/sounds/19458/ and licence :Creative Commons 0 License
+
+XYLOPHONE_SAMPLE = function () {
+ return "data:audio/wav;base64,xylophone.b64encoded";
+};
